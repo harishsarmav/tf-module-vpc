26d8ff4956a765ef5470b324aa57df6e9f2a9608
Hari <harishsarma.v@gmail.com>
2023-03-03 22:30:38 +0530

    updating tf-module-vpc

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   count       = length(var.subnets_cidr)
   vpc_id      = aws_vpc.main.id
-  cidr_block  = var.cidr_block[count.index]
+  cidr_block  = var.subnets_cidr[count.index]
 
   tags = merge(
     local.common_tags,
