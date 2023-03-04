562c7b44f27121023ae27cae73fb219444303cfa
Hari <harishsarma.v@gmail.com>
2023-03-04 19:22:47 +0530

    updating tf-module-vpc

--- main.tf
+++ main.tf
@@ -19,7 +19,11 @@ resource "aws_subnet" "main" {
 
 resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
-  peer_vpc_id   = "vpc-0416eb81280cc1347"
+  peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept   = true
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-peering"}
+  )
 }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,4 @@
 variable "cidr_block" {}
 variable "env" {}
 variable "subnets_cidr" {}
+variable "default_vpc_id" {}
