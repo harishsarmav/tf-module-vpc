215abbb326c0eb2fe5e374ce27844f5e7e776e3b
Hari <harishsarma.v@gmail.com>
2023-03-04 19:53:32 +0530

    updating tf-module-vpc

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_route" "default" {
 }
 
 resource "aws_route" "default-vpc" {
-  route_table_id = aws_vpc.main.default_route_table_id
-  destination_cidr_block = "172.31.0.0/16"
+  route_table_id = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
