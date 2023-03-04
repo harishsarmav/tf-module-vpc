6a62790c32caa84799d318ac81ddaad4f7bf8b11
Hari <harishsarma.v@gmail.com>
2023-03-04 19:15:48 +0530

    updating tf-module-vpc

--- main.tf
+++ main.tf
@@ -21,4 +21,5 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = "vpc-0416eb81280cc1347"
   vpc_id        = aws_vpc.main.id
+  auto_accept   = true
 }
