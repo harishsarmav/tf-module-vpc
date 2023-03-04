9ecb7e3304118eeeb6e324a9f7ded006c6570d0b
Hari <harishsarma.v@gmail.com>
2023-03-04 19:51:34 +0530

    updating tf-module-vpc

--- data.tf
+++ data.tf
@@ -1,1 +1,5 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_vpc" "default" {
+  id = var.default_vpc_id
+}
--- main.tf
+++ main.tf
@@ -28,8 +28,13 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
-
 resource "aws_route" "default" {
+  route_table_id = aws_vpc.main.default_route_table_id
+  destination_cidr_block = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_route" "default-vpc" {
   route_table_id = aws_vpc.main.default_route_table_id
   destination_cidr_block = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
