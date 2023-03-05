ecb6a49d88ca1da1b94279eb1715c1d4aa1c8698
Hari <harishsarma.v@gmail.com>
2023-03-05 21:15:39 +0530

    updating tf-module-vpc

--- main.tf
+++ main.tf
@@ -6,14 +6,25 @@ resource "aws_vpc" "main" {
   )
 }
 
-resource "aws_subnet" "main" {
-  count       = length(var.subnets_cidr)
+resource "aws_subnet" "public" {
+  count       = length(var.public_subnets_cidr)
   vpc_id      = aws_vpc.main.id
-  cidr_block  = var.subnets_cidr[count.index]
+  cidr_block  = var.public_subnets_cidr[count.index]
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-subnet-${count.index + 1}" }
+    { Name = "${var.env}-public-subnet-${count.index + 1}" }
+  )
+}
+
+resource "aws_subnet" "private" {
+  count       = length(var.private_subnets_cidr)
+  vpc_id      = aws_vpc.main.id
+  cidr_block  = var.private_subnets_cidr[count.index]
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-private-subnet-${count.index + 1}" }
   )
 }
 
@@ -28,24 +39,6 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
-resource "aws_route" "default" {
-  route_table_id            = aws_vpc.main.default_route_table_id
-  destination_cidr_block    = "172.31.0.0/16"
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
-resource "aws_route" "igw-route" {
-  route_table_id            = aws_vpc.main.default_route_table_id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                = aws_internet_gateway.igw.id
-}
-
-resource "aws_route" "default-vpc" {
-  route_table_id            = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block    = var.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -55,49 +48,73 @@ resource "aws_internet_gateway" "igw" {
   )
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-public-route-table"}
+  )
+}
+
+resource "aws_route_table_association" "public-rt-assoc" {
+  count               = length(aws_subnet.public)
+  subnet_id           = aws_subnet.public.*.id[count.index]
+  route_table_id      = aws_route_table.public.id
+}
 
 // create EC2
 
-data "aws_ami" "centos8" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
-}
-
-resource "aws_instance" "web" {
-  ami                     = data.aws_ami.centos8.id
-  instance_type           = "t3.micro"
-  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
-  subnet_id               = aws_subnet.main.*.id[0]
-
-
-  tags = {
-    Name = "test-centos8"
-  }
-}
-
-resource "aws_security_group" "allow_tls" {
-  name          = "allow_tls"
-  description   = "Allow TLS inbound traffic"
-  vpc_id        = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port = 22
-    protocol  = "tcp"
-    to_port   = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
+#data "aws_ami" "centos8" {
+#  most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#  owners      = ["973714476881"]
+#}
+#
+#resource "aws_instance" "web" {
+#  ami                     = data.aws_ami.centos8.id
+#  instance_type           = "t3.micro"
+#  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
+#  subnet_id               = aws_subnet.main.*.id[0]
+#
+#
+#  tags = {
+#    Name = "test-centos8"
+#  }
+#}
+#
+#resource "aws_security_group" "allow_tls" {
+#  name          = "allow_tls"
+#  description   = "Allow TLS inbound traffic"
+#  vpc_id        = aws_vpc.main.id
+#
+#  ingress {
+#    description = "TLS from VPC"
+#    from_port = 22
+#    protocol  = "tcp"
+#    to_port   = 22
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port = 0
+#    protocol  = "-1"
+#    to_port   = 0
+#    cidr_blocks = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#
+#  tags = {
+#    Name = "allow_tls"
+#  }
+#}
--- vars.tf
+++ vars.tf
@@ -1,4 +1,5 @@
 variable "cidr_block" {}
 variable "env" {}
-variable "subnets_cidr" {}
+variable "public_subnets_cidr" {}
+variable "private_subnets_cidr" {}
 variable "default_vpc_id" {}
