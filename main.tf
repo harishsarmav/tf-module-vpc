f375348990fd9756f0865cf13d10919db9d6c6cd
Hari <harishsarma.v@gmail.com>
2023-03-05 21:45:36 +0530

    updating tf-module-vpc

--- main.tf
+++ main.tf
@@ -117,46 +117,45 @@ resource "aws_route_table_association" "private-rt-assoc" {
 
 // create EC2
 
-#data "aws_ami" "centos8" {
-#  most_recent = true
-#  name_regex  = "Centos-8-DevOps-Practice"
-#  owners      = ["973714476881"]
-#}
-#
-#resource "aws_instance" "web" {
-#  ami                     = data.aws_ami.centos8.id
-#  instance_type           = "t3.micro"
-#  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
-#  subnet_id               = aws_subnet.main.*.id[0]
-#
-#
-#  tags = {
-#    Name = "test-centos8"
-#  }
-#}
-#
-#resource "aws_security_group" "allow_tls" {
-#  name          = "allow_tls"
-#  description   = "Allow TLS inbound traffic"
-#  vpc_id        = aws_vpc.main.id
-#
-#  ingress {
-#    description = "TLS from VPC"
-#    from_port = 22
-#    protocol  = "tcp"
-#    to_port   = 22
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port = 0
-#    protocol  = "-1"
-#    to_port   = 0
-#    cidr_blocks = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#
-#  tags = {
-#    Name = "allow_tls"
-#  }
-#}
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  ami                     = data.aws_ami.centos8.id
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
+  subnet_id               = aws_subnet.private.*.id[0]
+  
+  tags = {
+    Name = "test-centos8"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name          = "allow_tls"
+  description   = "Allow TLS inbound traffic"
+  vpc_id        = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
