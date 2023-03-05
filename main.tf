ad3dca0a35e7f670b501a0bba38ad2c85fc38fd5
Hari <harishsarma.v@gmail.com>
2023-03-05 13:58:21 +0530

    updating tf-module-vpc

--- main.tf
+++ main.tf
@@ -29,14 +29,20 @@ resource "aws_vpc_peering_connection" "peer" {
 }
 
 resource "aws_route" "default" {
-  route_table_id = aws_vpc.main.default_route_table_id
-  destination_cidr_block = "172.31.0.0/16"
+  route_table_id            = aws_vpc.main.default_route_table_id
+  destination_cidr_block    = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
+resource "aws_route" "igw-route" {
+  route_table_id            = aws_vpc.main.default_route_table_id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = "aws_internet_gateway.igw.id"
+}
+
 resource "aws_route" "default-vpc" {
-  route_table_id = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block = var.cidr_block
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
