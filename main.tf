440ec2f9992170653f341ef8ff56ac19f73ebcec
Hari <harishsarma.v@gmail.com>
2023-03-06 16:53:52 +0530

    updating tf-module-vpc

--- main.tf
+++ main.tf
@@ -156,6 +156,14 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    protocol  = "tcp"
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     protocol  = "-1"
