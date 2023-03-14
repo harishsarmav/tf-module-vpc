b1acac44537e9c7c220c4497c52e66a9b5ec3905
Hari <harishsarma.v@gmail.com>
2023-03-14 20:41:36 +0530

    updating tf-module-vpc

--- subnets.tf
+++ subnets.tf
@@ -10,13 +10,14 @@ module "public_subnets" {
   cidr_block  = each.value.cidr_block
   name        = each.value.name
   internet_gw = lookup(each.value, "internet_gw", false)
-  #  nat_gw                      = lookup(each.value, "nat_gw", false)
+  nat_gw                      = lookup(each.value, "nat_gw", false)
 
   ##  Local resources which are created
   vpc_id                    = aws_vpc.main.id
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   tags                      = local.common_tags
   gateway_id                = aws_internet_gateway.gw.id
+  nat_gw_id                   = aws_nat_gateway.ngw.id
 }
 
 module "private_subnets" {
