d41132de9f97519fa9e2a45a80095a4655a509f7
Hari <harishsarma.v@gmail.com>
2023-03-15 08:20:25 +0530

    updating tf-module-vpc

--- subnets.tf
+++ subnets.tf
@@ -17,7 +17,6 @@ module "public_subnets" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   tags                      = local.common_tags
   gateway_id                = aws_internet_gateway.gw.id
-  nat_gw_id                   = aws_nat_gateway.ngw.id
 }
 
 module "private_subnets" {
@@ -38,6 +37,5 @@ module "private_subnets" {
   vpc_id                      = aws_vpc.main.id
   vpc_peering_connection_id   = aws_vpc_peering_connection.peer.id
   tags                        = local.common_tags
-  gateway_id                  = aws_internet_gateway.gw.id
   nat_gw_id                   = aws_nat_gateway.ngw.id
 }
