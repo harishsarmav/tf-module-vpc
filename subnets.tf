2dbb971bf1416e312f6f79a52070f8cfa7886432
Hari <harishsarma.v@gmail.com>
2023-03-15 07:52:08 +0530

    updating tf-module-vpc

--- subnets.tf
+++ subnets.tf
@@ -10,7 +10,7 @@ module "public_subnets" {
   cidr_block  = each.value.cidr_block
   name        = each.value.name
   internet_gw = lookup(each.value, "internet_gw", false)
-  nat_gw                      = lookup(each.value, "nat_gw", false)
+  nat_gw      = lookup(each.value, "nat_gw", false)
 
   ##  Local resources which are created
   vpc_id                    = aws_vpc.main.id
